afb762925d91df1a4d01501e86dc30095792c8db
DebHtm <yolm9yolm@gmail.com>
2025-08-22 19:26:15 +0300

    DOM2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+const listItems = document.querySelectorAll('li');
+const listParent = listItems[0].parentElement;
+
+function sorting(items) {
+  return [...items].sort((a, b) => {
+    const aSalary = Number(a.dataset.salary.replace(/[$,]/g, ''));
+    const bSalary = Number(b.dataset.salary.replace(/[$,]/g, ''));
+
+    return bSalary - aSalary;
+  });
+}
+
+function sortName(parentEl, sortedItems) {
+  parentEl.innerHTML = '';
+  sortedItems.forEach((li) => parentEl.appendChild(li));
+}
+
+const sortedArr = sorting(list);
+
+sortName(listParent, sortedArr);
